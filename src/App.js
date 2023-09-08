dc8de15921f24b2aad8e2c608d955c8ae88e03de
Vincent Zerbib <contact@zelabstudio.com>
2023-09-08 09:47:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let user = {
+    firstName: "Vincent",
+    lastName: "Zerbib",
+    isAuth: false
+  }
+
+  const { firstName, lastName, isAuth } = user;
+
+  const authenticateUser = () => {
+    console.log("trigger");
+    user.isAuth = true
+  }
+
+  return (
+    <header>
+      <p>
+        Bonjour {firstName} {lastName}
+      </p>
+      <p>
+        {
+          isAuth ?
+            (<p>Vous êtes connecté</p>)
+            : (<div>
+              <p>Vous n'êtes pas connecté</p>
+              <button onClick={() => authenticateUser()}>Connexion</button>
+            </div>)
+        }
+      </p>
+    </header>
+  );
+}
+
+export default App;
